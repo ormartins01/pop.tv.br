231feb7fcec6ef9a069f9e2a9db939e431702efe
Ricardo Martins <102545943+ormartins01@users.noreply.github.com>
2024-05-04 16:17:47 -0300

    fix: link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if (Hls.isSupported()) {
   let video = document.getElementById("video");
-  let videoLink = "http://localhost:8080/hls/test.m3u8";
+  let videoLink = "http://srv26513565.ultasrv.com:8080/hls/test.m3u8";
   let hls = new Hls();
   hls.loadSource(videoLink);
   hls.attachMedia(video);
