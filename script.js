d63c10b2edf561fab713326ab104a9fe76a2438b
Ricardo Martins <102545943+ormartins01@users.noreply.github.com>
2024-05-11 00:19:50 -0300

    feat: others pages

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 if (Hls.isSupported()) {
-  let video = document.getElementById("video");
+  let video = document.getElementById("video-so");
   let videoLink = "http://localhost:8080/hls/test.m3u8";
+  let hls = new Hls();
+  hls.loadSource(videoLink);
+  hls.attachMedia(video);
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    video.play();
+  });
+}
+if (Hls.isSupported()) {
+  let video = document.getElementById("video-pa");
+  let videoLink = "http://localhost:8081/hls/test.m3u8";
   let hls = new Hls();
   hls.loadSource(videoLink);
   hls.attachMedia(video);
